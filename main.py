fd16d17a2aa837e7e46f29b0b95ca1936f25475c
Zinath09 <75094119+Zinath09@users.noreply.github.com>
2023-06-11 23:56:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,39 +5,32 @@ from cassandra.cluster import Cluster
 
 def show(r):
     for i in r:
-        # print(i)
+        print(i)
         for name, el in zip(i._fields,i):
             print(name, " ", el)
         print()
-
-def getAtribute( sth, atribute = None):
-    for i in sth:
-        for name, el in zip(i._fields,i):
-            if atribute is None or atribute ==name:
-                return el
-    assert("GETTING ERROR")
 
 def returnLists(session):
     clients = []
 
     with open("data/clients.csv") as f:
         data = csv.DictReader(f)
-        for row in (data):
+        for i, row in enumerate(data):
             name = row["name"]
             surname = row["surname"]
             library = row["library"]
-            id = getAtribute(session.execute(f"SELECT * from clients where name = '{name}' AND surname = '{surname}' ALLOW FILTERING;"), "id")
-            clients.append(Client(name, surname, library, id, session))
+            clients.append(Client(name, surname, library, session))
 
     show(session.execute("SELECT * from clients;"))
     libraries = []
 
+
     with open("data/libraries.csv") as f:
         data = csv.DictReader(f)
-        for row in (data):
+        for i, row in enumerate(data):
             name = row["name"]
-            id = getAtribute(session.execute(f"SELECT * from libraries where name = '{name}' ALLOW FILTERING;"), "id")
-            libraries.append(Library(name, id, session))
+            libraries.append(Library(name, session))
+
     return clients, libraries
 
 if __name__ == "__main__":
@@ -45,28 +38,18 @@ if __name__ == "__main__":
     session = cluster.connect("libraries_system")   
 
     clients, libraries = returnLists(session)
-
     
-    book_title = "Geometria analityczna"
+
+    book_title = "Pan Tadeusz"
+    show(session.execute('SELECT * FROM libraries;'))
+    libraries[0].removeBook(book_title)
 
     libraries[0].addBook(book_title)
     libraries[1].addBook(book_title)
 
     clients[1].lendBook(book_title)
-    # clients[1].returnBook(book_title)
-
-    print(clients[1].id)
-
-    print("shelf_")
-    for l in clients:
-        print("client: ",l.id)
-        show(session.execute(f"SELECT * FROM shelf_{l.id};"))    
-
+    clients[1].returnBook(book_title)
+    
     libraries[0].removeBook(book_title)
-    libraries[1].removeBook(book_title)
 
     cluster.shutdown
-
-
-
-  
